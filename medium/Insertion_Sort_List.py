074482b3e816f3aaa141c94357fea7703677d33d
junghyun4425 <junghyun153@naver.com>
2021-02-11 17:44:23 +0900

    Solved:Insertion_Sort_List/medium

--- medium/Insertion_Sort_List.py
+++ medium/Insertion_Sort_List.py
@@ -9,6 +9,13 @@ answer: 1 -> 2 -> 3 -> 4
 시간제한이 없길래 그냥 O(n^2)으로 풀면 된다고 생각해서 while문과 재귀함수를 하나씩 사용해서 문제를 해결.
 그런데 time limit 이 걸려있어서 실패. 아무래도 바꾸려는 노드의 값보다 작은값을 만나면 break 해주길 원해는 모양.
 다른 공부도 해야하기 때문에 일단 구현한 코드는 올려놓고 내일 prev, next 두개의 포인터로 break point를 찾으면서 처음부터 다시 풀 생각.
+
+두번째 시도.
+start 라는 더미 노드로 시작. (prev, next 포인터 두개를 사용)
+start부터 하나씩 붙여가고, prev와 next는 start에서 항상 시작해 값을 비교해 나감.
+next가 현재 노드보다 큰 경우엔 그자리를 바꿔주면 됨. (start 노드에 붙여주기)
+이런식으로 start 노드를 완성시켜서 정렬을 완료.
+중요한건 time limit을 만족하냐 못하냐 이기 때문에 쓸데없는 계산을 피하기 위해 도중에 바로 break문으로 나갈 수 있게끔 설계.
 '''
 
 # Definition for singly-linked list.
@@ -16,6 +23,23 @@ answer: 1 -> 2 -> 3 -> 4
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Passed Solution
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        start = ListNode(-1)
+        while head:
+            next_head = head.next
+            prev, nxt = start, start.next
+            while nxt:
+                if nxt.val > head.val:
+                    break
+                prev = nxt
+                nxt = nxt.next
+            head.next = nxt
+            prev.next = head
+            head = next_head
+        return start.next
 
 # Failed Solution (Time limit)
 class Solution:
