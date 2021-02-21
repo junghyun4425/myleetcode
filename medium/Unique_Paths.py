f477dccb118601fa61e47a5c79b0813c4c8866e4
junghyun4425 <junghyun153@naver.com>
2021-02-22 06:25:37 +0900

    Review:Unique_Paths/medium

--- medium/Unique_Paths.py
+++ medium/Unique_Paths.py
@@ -10,8 +10,25 @@ answer: 28
 하지만 DP로도 풀 수 있는것이 생각났기에 익숙치 않은 방법으로 풀기로 결정.
 우선, (x,0) (0,y) 에서 모든 x와 y값에 대해 1로 정의할 수 있음. ((0,0)에서 로봇이 갈수있는 경로는 오직 하나뿐이므로)
 dp(m,n) = dp(m-1,n) + dp(m,n-1) 라는 간단한 점화식을 구할 수 있고, 그대로 구현하면 해결됨.
+
+Review
+이차원 배열의 DP를 일차원으로 구현이 가능. (이전값을 참조할때 현재위치 기준으로 왼쪽과 상단을 더해주기 때문에 모든 향을 따로 저장할 필요 없음)
+따라서 점화식은,
+dp[i] = dp[i] + dp[i-1]
+로 계산하면 되는 간단한 문제.
 '''
 
+# Second Try (Optimized Space complexity O(mn) to O(n))
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for _ in range(1, m):
+            for i in range(1, n):
+                dp[i] += dp[i-1]
+        return dp[-1]
+
+# First Try
+'''
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[1] * n for _ in range(m)]
@@ -19,3 +36,4 @@ class Solution:
             for j in range(1, n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[-1][-1]
+'''
